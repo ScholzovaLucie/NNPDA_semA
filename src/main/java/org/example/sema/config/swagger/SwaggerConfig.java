921209704185583c8c7c3757bb07e45d91b26c7b
ScholzovaLucie <lucka.scholz@gmial.com>
2024-10-17 10:07:13 +0200

    feat: auth

--- src/main/java/org/example/sema/config/swagger/SwaggerConfig.java
+++ src/main/java/org/example/sema/config/swagger/SwaggerConfig.java
@@ -1,20 +1,18 @@
 package org.example.sema.config.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI defineOpenApi() {
-        Server server = new Server();
-        server.setUrl("http://localhost:2024");
 
         Contact myContact = new Contact();
         myContact.setName("Lucie Scholzová");
@@ -24,6 +22,13 @@ public class SwaggerConfig {
                 .version("1.0")
                 .description("This API exposes endpoints for NNPDA SEM A.")
                 .contact(myContact);
-        return new OpenAPI().info(information).servers(List.of(server));
+        return new OpenAPI().info(information)
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth")).components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("Bearer")
+                                        .bearerFormat("JWT")));
     }
 }
